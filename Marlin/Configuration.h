811cc0ddf1175d79afa8c2873502d0e38c47fa7b
slasherTripl <misha.leuta@gmail.com>
2022-06-16 21:21:27 +0300

    fucking thermistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 107.3 }
 
 /**
  * Default Max Feed Rate (mm/s)
