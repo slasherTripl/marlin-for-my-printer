a1feb007a4c79d196f55bc54152c4616e4a41767
slasherTripl <misha.leuta@gmail.com>
2022-06-25 22:55:24 +0300

    calibrated axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 107.3 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 86, 82, 205.1, 107.3 }
 
 /**
  * Default Max Feed Rate (mm/s)
